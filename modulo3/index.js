d06da3b3dcf8591131cab4ced042142be3454e70
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2020-07-12 19:15:48 -0300

    Join data to an element

---
+++ modulo3/index.js
@@ -0,0 +1,16 @@
+const data = [
+    { width: 200, height: 100, fill: 'green' }
+]
+const svg = d3.select('svg')
+
+const rect = svg.select('rect')
+    .data(data)
+    .attr('width', function (d) {
+        return d.width
+    })
+    .attr('height', function (d) {
+        return d.height
+    })
+    .attr('fill', function (d) {
+        return d.fill
+    });
