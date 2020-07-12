95786455f5e90ed06cdefe0fad1ce1599bf074f8
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2020-07-12 20:39:32 -0300

    Creating Axis

--- modulo4/index.js
+++ modulo4/index.js
@@ -10,10 +10,16 @@ const margin = { top: 20, right: 20, bottom: 100, left: 100 };
 const graphWidth = 600 - margin.left - margin.right;
 const graphHeight = 600 - margin.top - margin.bottom;
 
+// Create Graph Group
 const graph = svg.append('g')
     .attr('width', graphWidth)
     .attr('height', graphHeight)
     .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+// Create Axis Group
+const xAxisGroup = graph.append('g')
+    .attr('transform', `translate(0, ${graphHeight})`);
+const yAxisGroup = graph.append('g');
 
 d3.json('menu.json').then(data => {
 
@@ -21,7 +27,7 @@ d3.json('menu.json').then(data => {
     // Create Scale
     const y = d3.scaleLinear()
         .domain([0, max])
-        .range([0, 500]);
+        .range([0, graphHeight]);
 
     const x = d3.scaleBand()
         .domain(data.map(item => item.name))
@@ -31,12 +37,12 @@ d3.json('menu.json').then(data => {
 
     // join the data to rects
     const rects = graph.selectAll('rect')
-        .data(data)
+        .data(data);
 
     rects.attr('width', x.bandwidth)
         .attr('height', d => y(d.orders))
         .attr('fill', 'orange')
-        .attr('x', d => x(d.name))
+        .attr('x', d => x(d.name));
 
     //append the enter selection to the DOM
     rects.enter()
@@ -44,7 +50,13 @@ d3.json('menu.json').then(data => {
         .attr('width', x.bandwidth)
         .attr('height', d => y(d.orders))
         .attr('fill', 'orange')
-        .attr('x', d => x(d.name))
+        .attr('x', d => x(d.name));
+    // Create Axis
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
+
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
 
 
 })
