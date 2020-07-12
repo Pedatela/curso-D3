1d9f8e206d3353d5adc0d9c70d71e7f712181e6c
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2020-07-12 20:51:49 -0300

    Tick Formatting

--- modulo4/index.js
+++ modulo4/index.js
@@ -56,10 +56,15 @@ d3.json('menu.json').then(data => {
 
     // Create Axis
     const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y);
+    const yAxis = d3.axisLeft(y)
+        .ticks(3)
+        .tickFormat(d => d + ' orders');
 
     xAxisGroup.call(xAxis);
     yAxisGroup.call(yAxis);
 
-
+    xAxisGroup.selectAll('text')
+        .attr('transform', 'rotate(-40)')
+        .attr('text-anchor', 'end')
+        .attr('fill', 'orange');
 })
