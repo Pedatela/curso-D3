0a63a5573c0cf245edd55d604a0b5ab1eb08e7be
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2020-07-12 18:53:40 -0300

    Adding Svg`s text

--- modulo2/index.js
+++ modulo2/index.js
@@ -15,11 +15,19 @@ svg.append('circle')
     .attr('r', 50)
     .attr('cx', 300)
     .attr('cy', 70)
-    .attr('fill', 'pink')
+    .attr('fill', 'pink');
 
 svg.append('line')
     .attr('x1', 370)
     .attr('x2', 400)
     .attr('y1', 20)
     .attr('y2', 120)
-    .attr('stroke', 'red')
+    .attr('stroke', 'red');
+
+
+svg.append('text')
+    .attr('x', 20)
+    .attr('y', 200)
+    .attr('fill', 'grey')
+    .text('hello, ninjas!')
+    .style('font-family', 'arial');
