f3d913ae2724f55aed39a6a7cf0d3deb87704e2f
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2020-07-12 19:24:23 -0300

    Joinig data to multiple elements

--- modulo3/index.js
+++ modulo3/index.js
@@ -1,9 +1,11 @@
 const data = [
-    { width: 200, height: 100, fill: 'green' }
+    { width: 200, height: 100, fill: 'purple' },
+    { width: 100, height: 60, fill: 'pink' },
+    { width: 50, height: 30, fill: 'red' }
 ]
 const svg = d3.select('svg')
 
-const rect = svg.select('rect')
+const rect = svg.selectAll('rect')
     .data(data)
     .attr('width', d => d.width)
     .attr('height', d => d.height)
