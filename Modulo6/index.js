74f757de9e59155b54932be2c1be7833cf9a2b4c
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2020-07-13 13:42:21 -0300

    Listen to Firebase Changes

--- Modulo6/index.js
+++ Modulo6/index.js
@@ -76,12 +76,25 @@ const update = (data) => {
 }
 
 let data = []
-
 db.collection('dishes').onSnapshot(res => {
     res.docChanges().forEach(change => {
-        console.log(change.type, change.doc.data())
+        const doc = { ...change.doc.data(), id: change.doc.id }
+        switch (change.type) {
+            case 'added':
+                data.push(doc);
+                break;
+            case 'modified':
+                const index = data.findIndex(item => item.id == doc.id)
+                data[index] = doc
+                break;
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id)
+                break;
+            default:
+                break;
+        }
     });
-    // update()
+    update(data)
 });
 
 
