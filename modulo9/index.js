15065b758844c9a582e32751114956f08e99cd27
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2020-07-15 19:50:27 -0300

    Adding data to firestore

--- modulo9/index.js
+++ modulo9/index.js
@@ -24,3 +24,23 @@ btns.forEach(btn => {
 
     })
 });
+
+// form submit
+form.addEventListener('submit', e => {
+    // prevent default action
+    e.preventDefault();
+
+    const distance = parseInt(input.value)
+    if (distance) {
+        db.collection('activities').add({
+            distance,
+            activity,
+            date: new Date().toString()
+        }).then(() => {
+            error.textContent = '';
+            input.value = '';
+        })
+    } else {
+        error.textContent = 'Please enter a valid distance';
+    }
+})
